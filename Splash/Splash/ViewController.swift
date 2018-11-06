08e26651467b84a5737b84c374e6cdd86c48c865
Alex Appel <alex.appel97@gmail.com>
2018-11-05 21:56:35 -0600

    Create Xcode project

---
+++ Splash/Splash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splash
+//
+//  Created by Alex Appel on 11/5/18.
+//  Copyright © 2018 Alex Appel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
